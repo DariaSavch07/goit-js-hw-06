573f91f889035f108e4da5b17c0e3d2ae2f41950
Daria Savchenko <myworkpl24@gmail.com>
2025-04-14 11:23:08 +0200

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,43 +1,39 @@
 
-const sortByDescendingFriendCount = users =>
-  [...users].sort((a, b) => b.friends.length - a.friends.length);
+class StringBuilder {
+  // Приватна властивість
+  #value;
 
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
-  ])
-);
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  // Повертає поточне значення
+  getValue() {
+    return this.#value;
+  }
+
+  // Додає рядок в кінець
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  // Додає рядок на початок
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  // Додає рядок з обох боків
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+// Перевірка
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
